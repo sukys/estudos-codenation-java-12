b2c8d04b1701f8e87e166c24feaaad6478fb891b
Luis Sukys <luis.sukys@sippulse.com>
2020-07-31 15:16:56 -0300

    correção da permissão de websecurity

--- src/main/java/com/challenge/config/SecurityConfiguration.java
+++ src/main/java/com/challenge/config/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/***", "/configuration/security",
-				"/swagger-ui;html", "/webjars/**", "/user*");
+				"/swagger-ui;html", "/webjars/**", "/user");
 	}
 
 	@Override
